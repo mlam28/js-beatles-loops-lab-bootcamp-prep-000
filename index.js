132f533830a43fa1e42a54d94607d468b36c3354
Michelle Lam <shelly2.lam@gmail.com>
2019-05-20 15:04:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var musicians = ["John Lennon", "Paul McCartney",  "George Harrison", "Ringo Starr"]
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
  
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = [];
@@ -11,6 +18,7 @@ function theBeatlesPlay(musicians, instruments) {
   return emptyArray
 }
 
-function johnLennonFacts(arrayfacts) {
-  
+function johnLennonFacts(facts) {
+  var exclamation = []
+  while()
 }
